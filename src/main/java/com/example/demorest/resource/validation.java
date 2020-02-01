cdde1951262c40766d21814170d6f90cf22458a8
sunilvarma <sunilvarma@sunil>
2020-02-01 19:57:29 +0530

    @2/1/2020

---
+++ src/main/java/com/example/demorest/resource/validation.java
@@ -0,0 +1,24 @@
+package com.example.demorest.resource;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demorest.vo.User;
+
+@RestController		//this gives response of each method in json
+@RequestMapping("/database/")
+public class validation {
+	
+	//@ResponseBody  //this annotation is not required when restcontroller annotation is represented in class level
+	@PostMapping(path="valid", consumes = "application/json", produces = "application/json")
+	public Boolean valid(@RequestBody User user) {
+
+		if (user.getUsername().equals("username") && user.getPassword().equals("password"))
+			return true;
+		else
+			return false;
+
+	}
+}

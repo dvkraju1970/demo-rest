45d424c6fa0dfaaa02d4e91940fd241adafab2cc
sunilvarma <sunilvarma@sunil>
2020-03-20 00:30:11 +0530

    jwt configuration done partially

--- src/main/java/com/example/demorest/resource/validation.java
+++ src/main/java/com/example/demorest/resource/validation.java
@@ -1,5 +1,6 @@
 package com.example.demorest.resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +21,11 @@ public class validation {
 		else
 			return false;
 
+	}
+	
+	@GetMapping(path="va")
+	public String view() {
+
+		return "hello world";
 	}
 }
